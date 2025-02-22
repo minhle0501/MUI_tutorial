362098d3b2b4f3bf107e6db7f7cd090ab9345677
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-02-22 17:41:38 +0700

    text field select radio buttton checkbox & switch

--- mui-tutorial/src/App.tsx
+++ mui-tutorial/src/App.tsx
@@ -1,11 +1,22 @@
-import MuiButton from "./components/MuiButton";
+import MuiSwitch from "./components/MuiSwitch";
+//import MuiCheckBox from "./components/MuiCheckBox";
+//import MuRadioButton from "./components/MuRadioButton";
+//import MuiSelect from "./components/MuiSelect";
+//import MuiTextField from "./components/MuiTextField";
+//import MuiButton from "./components/MuiButton";
 //import MuiTypography from "./components/MuiTypography";
 import "./App.css";
+
 
 function App() {
   return <div className="App">
     {/* <MuiTypography /> */}
-   <MuiButton/>
+   {/* <MuiButton/> */}
+  {/*  <MuiTextField/> */}
+  {/* <MuiSelect/> */}
+  {/* <MuRadioButton/> */}
+  {/* <MuiCheckBox/> */}
+  <MuiSwitch/>
   </div>;
 }
 
