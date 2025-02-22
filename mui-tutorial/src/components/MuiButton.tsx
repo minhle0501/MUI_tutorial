5f81de0918a9d4f6cd605e1f7741b0745232f710
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-02-22 13:09:52 +0700

    ToggleButton

--- mui-tutorial/src/components/MuiButton.tsx
+++ mui-tutorial/src/components/MuiButton.tsx
@@ -1,7 +1,27 @@
-import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import {
+  FormatBold,
+  FormatItalic,
+  FormatUnderlined,
+} from "@mui/icons-material";
+import { useState } from "react";
 
 const MuiButton = () => {
+    const [format, setFormat] = useState<string[]|null>(null);
+    console.log({format})
+    const handleFormat = (
+      _event: React.MouseEvent<HTMLElement>,updatedFormat: string[] | null)=>{
+        setFormat(updatedFormat);
+      } 
+        
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction="row">
@@ -54,11 +74,30 @@ const MuiButton = () => {
         </IconButton>
       </Stack>
       <Stack direction="row">
-        <ButtonGroup variant="contained" orientation="vertical" size="small" color="secondary" aria-label="vertical contained secondary button group">
-          <Button >left</Button>
-          <Button >center</Button>
-          <Button >right</Button>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="vertical contained secondary button group"
+        >
+          <Button>left</Button>
+          <Button>center</Button>
+          <Button>right</Button>
         </ButtonGroup>
+      </Stack>
+      <Stack direction="row">
+        <ToggleButtonGroup aria-label="text formatting" value={format} onChange={handleFormat} size="small" color="success" exclusive>
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBold />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalic />
+          </ToggleButton>
+          <ToggleButton value="underline" aria-label="underline">
+            <FormatUnderlined />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
