7488e15efed15cdcf4b0f6f1b0629d6e2292ffab
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-02-24 15:25:25 +0700

    add Avatar component and change button to autocomplete in app

--- mui-tutorial/src/App.tsx
+++ mui-tutorial/src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Menu, MenuItem, Button } from '@mui/material';
-import { useState } from 'react';
-import MuiBottomNavigation from './components/MuiBottomNavigation';
-import MuiSpeedDial from './components/MuiSpeedDial';
-import MuiBreadcrumbs from './components/MuiBreadcrumbs';
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import { Autocomplete, TextField } from "@mui/material";
+import MuiAvatar from "./components/MuiAvatar";
+import MuiBottomNavigation from "./components/MuiBottomNavigation";
+import MuiSpeedDial from "./components/MuiSpeedDial";
+import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
 import MuiLink from "./components/MuiLink";
 import MuiNavBar from "./components/MuiNavBar";
 import MuiImageList from "./components/MuiImageList";
@@ -14,64 +14,54 @@ import MuiAutocomplete from "./components/MuiAutocomplete";
 import MuiRating from "./components/MuiRating";
 import MuiSwitch from "./components/MuiSwitch";
 import MuiCheckBox from "./components/MuiCheckBox";
-import MuRadioButton from "./components/MuRadioButton";
+import MuRadioButton from "./components/MuiRadioButton";
 import MuiSelect from "./components/MuiSelect";
 import MuiTextField from "./components/MuiTextField";
 import MuiButton from "./components/MuiButton";
 import MuiTypography from "./components/MuiTypography";
 
+const options = [
+  { label: "MuiAvatar", path: "/mui-avatar" },
+  { label: "MuiBottomNavigation", path: "/mui-bottom-navigation" },
+  { label: "MuiSpeedDial", path: "/mui-speed-dial" },
+  { label: "MuiBreadcrumbs", path: "/mui-breadcrumbs" },
+  { label: "MuiLink", path: "/mui-link" },
+  { label: "MuiNavBar", path: "/mui-nav-bar" },
+  { label: "MuiImageList", path: "/mui-image-list" },
+  { label: "MuiAccordion", path: "/mui-accordion" },
+  { label: "MuiCard", path: "/mui-card" },
+  { label: "MuiLayout", path: "/mui-layout" },
+  { label: "MuiAutocomplete", path: "/mui-autocomplete" },
+  { label: "MuiRating", path: "/mui-rating" },
+  { label: "MuiSwitch", path: "/mui-switch" },
+  { label: "MuiCheckBox", path: "/mui-check-box" },
+  { label: "MuiRadioButton", path: "/mui-radio-button" },
+  { label: "MuiSelect", path: "/mui-select" },
+  { label: "MuiTextField", path: "/mui-text-field" },
+  { label: "MuiButton", path: "/mui-button" },
+  { label: "MuiTypography", path: "/mui-typography" },
+];
+
 function App() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <BrowserRouter>
       <div>
         <h1>Thư viện Material-UI</h1>
-        <Button
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-        >
-          Open Menu
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
+        <Autocomplete
+          id="combo-box-demo"
+          options={options}
+          getOptionLabel={(option) => option.label}
+          style={{ width: 300, marginBottom: 20 }}
+          renderInput={(params) => <TextField {...params} label="Mui components" variant="outlined" />}
+          onChange={(_event, value) => {
+            if (value) {
+              window.location.href = value.path;
+            }
           }}
-        >
-          <MenuItem onClick={handleClose} component={Link} to="/mui-bottom-navigation">MuiBottomNavigation</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-speed-dial">MuiSpeedDial</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-breadcrumbs">MuiBreadCrumbs</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-link">MuiLink</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-nav-bar">MuiNavBar</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-image-list">MuiImageList</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-accordion">MuiAccordion</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-card">MuiCard</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-layout">MuiLayout</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-autocomplete">MuiAutocomplete</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-rating">MuiRating</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-switch">MuiSwitch</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-check-box">MuiCheckBox</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mu-radio-button">MuRadioButton</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-select">MuiSelect</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-text-field">MuiTextField</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-button">MuiButton</MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/mui-typography">MuiTypography</MenuItem>
-        </Menu>
+        />
         <Routes>
+          <Route path="/mui-avatar" element={<MuiAvatar />} />
           <Route path="/mui-bottom-navigation" element={<MuiBottomNavigation />} />
           <Route path="/mui-speed-dial" element={<MuiSpeedDial />} />
           <Route path="/mui-breadcrumbs" element={<MuiBreadcrumbs />} />
@@ -85,7 +75,7 @@ function App() {
           <Route path="/mui-rating" element={<MuiRating />} />
           <Route path="/mui-switch" element={<MuiSwitch />} />
           <Route path="/mui-check-box" element={<MuiCheckBox />} />
-          <Route path="/mu-radio-button" element={<MuRadioButton />} />
+          <Route path="/mui-radio-button" element={<MuRadioButton   />} />
           <Route path="/mui-select" element={<MuiSelect />} />
           <Route path="/mui-text-field" element={<MuiTextField />} />
           <Route path="/mui-button" element={<MuiButton />} />
