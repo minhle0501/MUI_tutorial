bdb196ff6657f0e45f33b03cf8e4590e75b4fa09
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-02-25 16:28:16 +0700

    DateTimePicker - useNavigate router

--- mui-tutorial/src/App.tsx
+++ mui-tutorial/src/App.tsx
@@ -1,5 +1,14 @@
-import { BrowserRouter, Routes, Route,} from "react-router-dom";
-import { Autocomplete, TextField } from "@mui/material";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import {
+  Autocomplete,
+  TextField,
+  Typography,
+  Box,
+} from "@mui/material";
+
+import MuiPicker from "./components/MuiPicker";
 import MuiLoadingButton from "./components/MuiLoadingButton";
 import MuiSkeleton from "./components/MuiSkeleton";
 import MuiProgress from "./components/MuiProgress";
@@ -25,18 +34,19 @@ import MuiAutocomplete from "./components/MuiAutocomplete";
 import MuiRating from "./components/MuiRating";
 import MuiSwitch from "./components/MuiSwitch";
 import MuiCheckBox from "./components/MuiCheckBox";
-import MuRadioButton from "./components/MuiRadioButton";
+import MuiRadioButton from "./components/MuiRadioButton";
 import MuiSelect from "./components/MuiSelect";
 import MuiTextField from "./components/MuiTextField";
 import MuiButton from "./components/MuiButton";
 import MuiTypography from "./components/MuiTypography";
 
 const options = [
-  {label: "MuiLoadingButton", path: "/mui-loading-button"},
-  {label: "MuiSkeleton", path: "/mui-skeleton"},
-  {label: "MuiProgress", path: "/mui-progress"},
-  {label: "MuiDiaLog", path: "/mui-dia-log"},
-  {label: "MuiSnackBar", path: "/mui-snack-bar"},
+  { label: "MuiPicker", path: "/mui-picker" },
+  { label: "MuiLoadingButton", path: "/mui-loading-button" },
+  { label: "MuiSkeleton", path: "/mui-skeleton" },
+  { label: "MuiProgress", path: "/mui-progress" },
+  { label: "MuiDiaLog", path: "/mui-dia-log" },
+  { label: "MuiSnackBar", path: "/mui-snack-bar" },
   { label: "MuiAlert", path: "/mui-alert" },
   { label: "MuiTable", path: "/mui-table" },
   { label: "MuiTooltip", path: "/mui-tooltip" },
@@ -65,57 +75,66 @@ const options = [
 ];
 
 function App() {
-
+  const navigate = useNavigate();
   return (
-    <BrowserRouter>
-      <div>
-        <h1>Thư viện Material-UI</h1>
-        <Autocomplete
-          id="combo-box-demo"
-          options={options}
-          getOptionLabel={(option) => option.label}
-          style={{ width: 300, marginBottom: 20 }}
-          renderInput={(params) => <TextField {...params} label="Mui components" variant="outlined" />}
-          onChange={(_event, value) => {
-            if (value) {
-              window.location.href = value.path;
-            }
-          }}
-        />
-        <Routes>
-          <Route path="/mui-loading-button" element={<MuiLoadingButton />} />
-          <Route path="/mui-skeleton" element={<MuiSkeleton />} />
-          <Route path="/mui-progress" element={<MuiProgress />} />
-          <Route path="/mui-dia-log" element={<MuiDiaLog />} />
-          <Route path="/mui-snack-bar" element={<MuiSnackBar />} />
-          <Route path="/mui-alert" element={<MuiAlert />} />
-          <Route path="/mui-table" element={<MuiTable />} />
-          <Route path="/mui-tooltip" element={<MuiTooltip />} />
-          <Route path="/mui-chip" element={<MuiChip />} />
-          <Route path="/mui-list" element={<MuiList />} />
-          <Route path="/mui-badge" element={<MuiBadge />} />
-          <Route path="/mui-avatar" element={<MuiAvatar />} />
-          <Route path="/mui-bottom-navigation" element={<MuiBottomNavigation />} />
-          <Route path="/mui-speed-dial" element={<MuiSpeedDial />} />
-          <Route path="/mui-breadcrumbs" element={<MuiBreadcrumbs />} />
-          <Route path="/mui-link" element={<MuiLink />} />
-          <Route path="/mui-nav-bar" element={<MuiNavBar />} />
-          <Route path="/mui-image-list" element={<MuiImageList />} />
-          <Route path="/mui-accordion" element={<MuiAccordion />} />
-          <Route path="/mui-card" element={<MuiCard />} />
-          <Route path="/mui-layout" element={<MuiLayout />} />
-          <Route path="/mui-autocomplete" element={<MuiAutocomplete />} />
-          <Route path="/mui-rating" element={<MuiRating />} />
-          <Route path="/mui-switch" element={<MuiSwitch />} />
-          <Route path="/mui-check-box" element={<MuiCheckBox />} />
-          <Route path="/mui-radio-button" element={<MuRadioButton   />} />
-          <Route path="/mui-select" element={<MuiSelect />} />
-          <Route path="/mui-text-field" element={<MuiTextField />} />
-          <Route path="/mui-button" element={<MuiButton />} />
-          <Route path="/mui-typography" element={<MuiTypography />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Box component={"div"}>
+      <Typography variant="h2" component="h1">
+        Thư viện Material-UI
+      </Typography>
+      <Autocomplete
+        id="combo-box-demo"
+        options={options}
+        getOptionLabel={(option) => option.label}
+        style={{ width: 300, marginBottom: 20 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Mui components" variant="outlined" />
+        )}
+        onChange={(_event, value) => {
+          if (value) {
+            navigate(value.path);
+          }
+        }}
+      />
+
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Routes>
+            <Route path="/mui-picker" element={<MuiPicker />} />
+            <Route path="/mui-loading-button" element={<MuiLoadingButton />} />
+            <Route path="/mui-skeleton" element={<MuiSkeleton />} />
+            <Route path="/mui-progress" element={<MuiProgress />} />
+            <Route path="/mui-dia-log" element={<MuiDiaLog />} />
+            <Route path="/mui-snack-bar" element={<MuiSnackBar />} />
+            <Route path="/mui-alert" element={<MuiAlert />} />
+            <Route path="/mui-table" element={<MuiTable />} />
+            <Route path="/mui-tooltip" element={<MuiTooltip />} />
+            <Route path="/mui-chip" element={<MuiChip />} />
+            <Route path="/mui-list" element={<MuiList />} />
+            <Route path="/mui-badge" element={<MuiBadge />} />
+            <Route path="/mui-avatar" element={<MuiAvatar />} />
+            <Route
+              path="/mui-bottom-navigation"
+              element={<MuiBottomNavigation />}
+            />
+            <Route path="/mui-speed-dial" element={<MuiSpeedDial />} />
+            <Route path="/mui-breadcrumbs" element={<MuiBreadcrumbs />} />
+            <Route path="/mui-link" element={<MuiLink />} />
+            <Route path="/mui-nav-bar" element={<MuiNavBar />} />
+            <Route path="/mui-image-list" element={<MuiImageList />} />
+            <Route path="/mui-accordion" element={<MuiAccordion />} />
+            <Route path="/mui-card" element={<MuiCard />} />
+            <Route path="/mui-layout" element={<MuiLayout />} />
+            <Route path="/mui-autocomplete" element={<MuiAutocomplete />} />
+            <Route path="/mui-rating" element={<MuiRating />} />
+            <Route path="/mui-switch" element={<MuiSwitch />} />
+            <Route path="/mui-check-box" element={<MuiCheckBox />} />
+            <Route path="/mui-radio-button" element={<MuiRadioButton />} />
+            <Route path="/mui-select" element={<MuiSelect />} />
+            <Route path="/mui-text-field" element={<MuiTextField />} />
+            <Route path="/mui-button" element={<MuiButton />} />
+            <Route path="/mui-typography" element={<MuiTypography />} />
+          </Routes>
+      </LocalizationProvider>
+    </Box>
   );
 }
 
