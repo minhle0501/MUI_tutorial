a2716a870b202db715800be7098d7a11097464d0
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-03-15 16:39:27 +0700

    counter with persist(local storage)

--- mui-tutorial/src/pages/ZustandDemo.tsx
+++ mui-tutorial/src/pages/ZustandDemo.tsx
@@ -1,9 +1,12 @@
 import { Container } from "@mui/material";
 import ProductList from "../components/zustand_component/ProductList";
+import Counter from "../components/zustand_component/Counter";
+
 
 const ZustandDemo = () => {
   return (
     <Container>
+      <Counter />
       <ProductList />
     </Container>
   )
